6eb768f06f18906583d2ecaf65c11d6321bd2411
codedfinger <alexyikeh@gmail.com>
2023-02-19 01:40:37 +0100

    feature: frontend landing page, login and sugnup page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,42 @@
+import React from "react";
+import tw from "twin.macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import Hero from "components/hero/GetStarted.js";
+import Features from "components/features/HowTo.js";
+import ContactUsForm from "components/forms/ContactUs.js";
+import Footer from "components/footers/Footer.js";
+
+export default () => {
+  const Subheading = tw.span`uppercase tracking-widest font-bold text-primary-500`;
+  const HighlightedText = tw.span`text-primary-500`;
+
+  return (
+    <AnimationRevealPage>
+      <Hero roundedHeaderButton={true} />
+      <Features
+        subheading={<Subheading>Features</Subheading>}
+        heading={
+          <>
+            How you can use <HighlightedText>Taré.</HighlightedText>
+          </>
+        }
+      />
+      
+      {/* <FeatureWithSteps
+        subheading={<Subheading>STEPS</Subheading>}
+        heading={
+          <>
+            Easy to <HighlightedText>Get Started.</HighlightedText>
+          </>
+        }
+        textOnLeft={false}
+        imageSrc={macHeroScreenshotImageSrc}
+        imageDecoratorBlob={true}
+        decoratorBlobCss={tw`xl:w-40 xl:h-40 opacity-15 -translate-x-1/2 left-1/2`}
+      /> */}
+      <ContactUsForm />
+      <Footer />
+    </AnimationRevealPage>
+  );
+}
